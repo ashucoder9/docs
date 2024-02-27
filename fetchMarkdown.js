925b38e256e55ca0abeb511404f9ece3e5cba82d
Ashutosh Tripathi <ashucoder9@gmail.com>
2024-02-27 13:42:29 +0530

    new new

--- fetchMarkdown.js
+++ fetchMarkdown.js
@@ -5,7 +5,8 @@ import path from 'path';
 
 const filesToFetch = [
   {
-    url: 'https://raw.githubusercontent.com/ava-labs/teleporter/main/README.md',
+    rawUrl: 'https://raw.githubusercontent.com/ava-labs/teleporter/main/README.md', //mention full URL with file name
+    baseRepoUrl: 'https://github.com/ava-labs/teleporter/blob/main/', //mention base repo URL on github.com
     outputPath: './Teleporter/deep-dive.mdx',
     customContent: `---
 tags: [Avalanche Warp Messaging, AWM, Cross-Subnet Communication, Cross-Chain Communication]
@@ -29,18 +30,40 @@ async function ensureDirectoryExistence(filePath) {
   }
 }
 
-async function fetchAndSaveMarkdown({ url, outputPath, customContent }) {
+// Function to resolve relative links with absolute links
+function resolveRelativeLinks(content, baseRepoUrl) {
+  const markdownLinkRegex = /!\[.*?\]\((.*?)\)|\[.*?\]\((.*?)\)/g;
+
+  // Prefix relative links with baseRepoUrl
+  return content.replace(markdownLinkRegex, (match, imagePath, linkPath) => {
+    const relativePath = imagePath || linkPath;
+
+    if (!relativePath.startsWith('http')) {
+      // Ensure the baseRepoUrl format is correct for appending relative paths
+      const formattedBaseRepoUrl = baseRepoUrl.endsWith('/') ? baseRepoUrl : `${baseRepoUrl}/`;
+      return match.replace(relativePath, formattedBaseRepoUrl + relativePath);
+    }
+
+    return match;
+  });
+}
+
+async function fetchAndSaveMarkdown({ rawUrl, baseRepoUrl, outputPath, customContent }) {
   try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`Failed to fetch markdown file from ${url}`);
+    const response = await fetch(rawUrl);
+    if (!response.ok) throw new Error(`Failed to fetch markdown file from ${rawUrl}`);
 
     let content = await response.text();
 
+    // Prepend custom content to the fetched content
     content = customContent + content;
+
+    // Directly resolve relative links using baseRepoUrl
+    content = resolveRelativeLinks(content, baseRepoUrl);
 
     await ensureDirectoryExistence(outputPath);
     await fs.writeFile(outputPath, content);
-    console.log(`Markdown file from ${url} saved to ${outputPath}`);
+    console.log(`Markdown file fetched from ${rawUrl} and saved to ${outputPath}`);
   } catch (error) {
     console.error('Error:', error);
   }
