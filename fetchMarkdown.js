568534196b05376cbbcd33e2c6fd4f9e68e071bf
Ashutosh Tripathi <ashucoder9@gmail.com>
2024-02-29 19:27:37 +0530

    push changes

--- fetchMarkdown.js
+++ fetchMarkdown.js
@@ -5,16 +5,54 @@ import path from 'path';
 
 const filesToFetch = [
   {
-    rawUrl: 'https://raw.githubusercontent.com/ava-labs/teleporter/main/README.md', //mention full URL with file name
-    baseRepoUrl: 'https://github.com/ava-labs/teleporter/blob/main/', //mention base repo URL on github.com
-    outputPath: './Teleporter/deep-dive.mdx',
+    rawUrl: 'https://raw.githubusercontent.com/ava-labs/avalanchego/master/vms/platformvm/warp/README.md',
+    baseRepoUrl: 'https://github.com/ava-labs/avalanchego/tree/master/vms/platformvm/warp/',
+    outputPath: './cross-chain/awm/deep-dive.mdx',
     customContent: `---
-tags: [Avalanche Warp Messaging, AWM, Cross-Subnet Communication, Cross-Chain Communication]
-description: Avalanche Warp Messaging (AWM) provides a primitive for cross-subnet communication on the Avalanche Network.
-keywords: [ docs, documentation, avalanche, awm, cross-subnet communication, cross-chain, cross-chain communication ]
-sidebar_label: Deep Dive
-sidebar_position: 1
-title: Teleporter Deep Dive
+title: Deep Dive into AWM
+sidebarTitle: Deep Dive
+icon: searchengin
+---\n\n`
+  },
+  {
+    rawUrl: 'https://raw.githubusercontent.com/meaghanfitzgerald/coreth/docs-integration/precompile/contracts/warp/README.md',
+    baseRepoUrl: 'https://github.com/ava-labs/coreth/blob/master/precompile/contracts/warp/',
+    outputPath: './cross-chain/awm/evm-integration.mdx',
+    customContent: `---
+title: EVM Integration
+description: Avalanche Warp Messaging provides a basic primitive for signing and verifying messages between Subnets. The receiving network can verify whether an aggregation of signatures from a set of source Subnet validators represents a threshold of stake large enough for the receiving network to process the message. The Avalanche Warp Precompile enables this flow to send a message from blockchain A to blockchain B.
+sidebarTitle: EVM Integration
+icon: hive
+---\n\n`
+  },
+  {
+    rawUrl: 'https://raw.githubusercontent.com/ava-labs/teleporter/main/contracts/src/Teleporter/README.md',
+    baseRepoUrl: 'https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/',
+    outputPath: './cross-chain/Teleporter/overview.mdx',
+    customContent: `---
+title: Overview
+description: Teleporter is a messaging protocol built on top of Avalanche Warp Messaging that provides a developer-friendly interface for sending and receiving cross-chain messages from within the EVM.
+sidebarTitle: Overview
+icon: arrow-right-to-arc
+---\n\n`
+  },
+  {
+    rawUrl: 'https://raw.githubusercontent.com/ava-labs/teleporter/main/README.md',
+    baseRepoUrl: 'https://github.com/ava-labs/teleporter/blob/main/',
+    outputPath: './cross-chain/Teleporter/deep-dive.mdx',
+    customContent: `---
+title: Deep Dive into the Teleporter Protocol
+sidebarTitle: Deep Dive
+icon: searchengin
+---\n\n`
+  },
+  {
+    rawUrl: 'https://raw.githubusercontent.com/ava-labs/teleporter/main/contracts/src/CrossChainApplications/README.md',
+    baseRepoUrl: 'https://github.com/ava-labs/teleporter/blob/main/contracts/src/CrossChainApplications/',
+    outputPath: './cross-chain/Teleporter/examples.mdx',
+    customContent: `---
+sidebarTitle: Examples
+icon: window
 ---\n\n`
   }
 ];
